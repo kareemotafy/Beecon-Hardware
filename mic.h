54f4a94bae2f1a95f192e076707eace64fade825
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-02-11 00:00:50 +0200

    Created Mic module

---
+++ mic.h
@@ -0,0 +1,15 @@
+#ifndef MIC_MODULE_H
+#define MIC_MODULE_H
+
+class MicModule {
+public:
+    MicModule(int sampleWindow, int MIC_PIN);
+    int read_mic();
+
+private:
+    const int sampleWindow;
+    const int MIC_PIN;
+    unsigned int sample;
+};
+
+#endif // MIC_MODULE_H
