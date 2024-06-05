066fe372529a0e6198f8d6324416a20869af2c6d
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-06-06 00:16:23 +0300

    Modify fan run function to take addresses

--- fan.h
+++ fan.h
@@ -6,7 +6,7 @@ class Fan
 public:
     Fan(int FAN_PIN);
     void initialize();
-    void run(float temp, float heatThreshold, float coolThreshold);
+    void run(float &temp, float &heatThreshold, float &coolThreshold);
 
 private:
     const int FAN_PIN;
